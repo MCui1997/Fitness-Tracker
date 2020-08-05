fffb3344bba4451e0b3a4be5c5d28b752f8bf9b2
Michael Cui <cuimichael500@gmail.com>
2020-08-05 13:59:07 -0500

    added in api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,7 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-    // Used by api.js to get last workout
+    // get last workout
     app.get("/api/workouts", (req, res) => {
         db.Workout.find({})
         .then(workout => {
@@ -13,5 +13,54 @@ module.exports = function(app) {
         });
     });
     
-   
+    // creates new workout
+    app.post("/api/workouts", async (req, res)=> {
+        try{
+            const response = await db.Workout.create({type: "workout"})
+            res.json(response);
+        }
+        catch(err){
+            console.log("error occured", err);
+        }
+    })
+
+    // add exercises to workout
+    app.put("/api/workouts/:id", ({body, params}, res) => {
+
+        const workoutId = params.id;
+        let savedExercises = [];
+
+        // gets exercises within workout
+        db.Workout.find({_id: workoutId})
+            .then(dbWorkout => {
+                savedExercises = dbWorkout[0].exercises;
+                res.json(dbWorkout[0].exercises);
+                let allExercises = [...savedExercises, body]
+                console.log(allExercises)
+                updateWorkout(allExercises)
+            })
+            .catch(err => {
+                res.json(err);
+            });
+
+        function updateWorkout(exercises){
+            db.Workout.findByIdAndUpdate(workoutId, {exercises: exercises}, function(err, doc){
+            if(err){
+                console.log(err)
+            }
+
+            })
+        }
+            
+    })
+
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({})
+        .then(workout => {
+            res.json(workout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    }); 
 };
